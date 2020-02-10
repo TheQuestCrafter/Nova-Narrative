b0745ee10696b802df3522d1e3e9a444e7a7f609
Zoey Reyes <42983147+TheQuestCrafter@users.noreply.github.com>
2020-02-10 15:17:34 -0600

    Basic Forward Back Movement

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    Transform cameraRotation;
+    
+    [SerializeField]
+    private float rotationXSensitivity;
+    
+    [SerializeField]
+    private float rotationYSensitivity;
+
+    [SerializeField]
+    private Camera playerCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    private Rigidbody rb;
+
+    [SerializeField]
+    private float speed = 5;
+    [SerializeField]
+    private float turnSpeed;
+    [SerializeField]
+    private float maxSpeed;
+    [SerializeField]
+    private float currentVertSpeed;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        
+    }
+
+    private void Movement()
+    {
+        if (Input.GetButton("Vertical"))
+        {
+            VerticalMovement();
+        }
+        MouseLook();
+    }
+
+    private void MouseLook()
+    {
+        
+    }
+
+    private void VerticalMovement()
+    {
+        float verticalVelocity = Mathf.Clamp(speed * Input.GetAxisRaw("Vertical"), -maxSpeed, maxSpeed);
+        rb.AddForce(0, 0, verticalVelocity);
+        currentVertSpeed = rb.velocity.z; 
+    }
+}
